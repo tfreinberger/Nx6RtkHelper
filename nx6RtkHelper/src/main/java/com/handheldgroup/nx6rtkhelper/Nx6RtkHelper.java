40e210fb0c54c506724ef0039d44ea88c4905c03
Tobias Freinberger <t.freinberger@handheldgermany.com>
2023-06-05 16:50:47 +0200

    powerOn funtion

--- nx6RtkHelper/src/main/java/com/handheldgroup/nx6rtkhelper/Nx6RtkHelper.java
+++ nx6RtkHelper/src/main/java/com/handheldgroup/nx6rtkhelper/Nx6RtkHelper.java
@@ -1,4 +1,24 @@
 package com.handheldgroup.nx6rtkhelper;
 
+import android.annotation.SuppressLint;
+import android.os.IBinder;
+
+import com.mmi.IMmiDevice;
+
 public class Nx6RtkHelper {
+
+    public static void powerOn(boolean powerOn) {
+        try {
+            @SuppressLint("PrivateApi")
+            IBinder iBinder = (IBinder) Class.forName("android.os.ServiceManager")
+                    .getMethod("getService", String.class)
+                    .invoke(null, "mmi_device");
+            IMmiDevice mService = IMmiDevice.Stub.asInterface(iBinder);
+            //noinspection ConstantConditions
+            mService.writeForBackNode("/sys/class/ext_dev/function/ext_dev_5v_enable", powerOn ? "1" : "0");
+            mService.writeForBackNode("/sys/class/ext_dev/function/pin10_en", powerOn ? "1" : "0");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
