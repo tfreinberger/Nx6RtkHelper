39101c2ec5ff37494514a946ef6f6cd0fa2b5943
Tobias Freinberger <t.freinberger@handheldgermany.com>
2023-06-06 11:13:54 +0200

    v1.0.2

--- app/src/main/java/com/handheldgroup/nx6rtkhelper/MainActivity.java
+++ app/src/main/java/com/handheldgroup/nx6rtkhelper/MainActivity.java
@@ -1,9 +1,8 @@
 package com.handheldgroup.nx6rtkhelper;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
--- nx6RtkHelper/src/main/java/com/handheldgroup/nx6rtkhelper/Baudrate.java
+++ nx6RtkHelper/src/main/java/com/handheldgroup/nx6rtkhelper/Baudrate.java
@@ -2,5 +2,5 @@ package com.handheldgroup.nx6rtkhelper;
 
 public final class Baudrate {
     public static final int DEFAULT_F9P = 38400;
-    public static final int BAUD_115200 = 125200;
+    public static final int BAUD_115200 = 115200;
 }
